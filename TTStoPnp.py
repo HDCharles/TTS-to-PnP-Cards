5359248c6d99ae61bca9d8ebc6ad5c3938ec753a
HDCharles <39544797+HDCharles@users.noreply.github.com>
2020-03-26 18:05:58 -0400

    GUI update and made program more general

--- TTStoPnp.py
+++ TTStoPnp.py
@@ -9,42 +9,78 @@ import re
 import numpy
 
 top = tk.Tk()
-dirButtons = tk.Frame(top)
 top.title("TTStoPnp")
-text = tk.Text(top)
 
-
-# Code to add widgets will go here...
-
-imageHeight = 3640
+"""imageHeight = 3640
 imageWidth = 3720
 cardHeight = int(imageHeight / 7)
-cardWidth = int(imageWidth / 10)
+cardWidth = int(imageWidth / 10)"""
+
+# this is executed by default
+def useCurDir():
+    top.dir = os.getcwd()
+    # setupText()
+    setupSelector()
 
 
+def selectDir():
+    top.dir = tk.filedialog.askdirectory(initialdir="/", title="Select Directory",)
+    # setupText()
+    setupSelector()
+
+
+def setupText():
+    text.delete(1.0, tk.END)
+    text.insert(tk.INSERT, "dir: " + top.dir)
+    text.pack()
+
+    # setupSelector()
+
+
+def getImages():
+    directory = top.dir
+    imageList = []
+    for filename in os.listdir(directory):
+        if (
+            re.search(".*\.png", filename) or re.search(".*\.jpe{0,1}g", filename)
+        ) and not re.search("^sep.*", filename):
+            imagePath = os.path.join(directory, filename)
+            image = Image.open(imagePath)
+            if True:  # checkSize(image):
+                imageList.append(filename)
+    top.imageList = imageList
+
+
+"""
 def checkSize(image):
     width, height = image.size
-    return width == imageWidth and height == imageHeight
+    return width == imageWidth and height == imageHeight"""
 
 
-def nextCardLocation(cardCol, cardRow):
-    cardCol += 1
-    if cardCol == 3:
-        cardRow += 1
-        cardCol = 0
-    if cardRow == 3:
-        cardRow = 0
-    return cardCol, cardRow
+def separateImage(image, directory, filename, rows, cols):
+    def nextCardLocation(cardCol, cardRow):
+        cardCol += 1
+        if cardCol == 3:
+            cardRow += 1
+            cardCol = 0
+        if cardRow == 3:
+            cardRow = 0
+        return cardCol, cardRow
 
+    imageWidth, imageHeight = image.size
 
-def separateImage(image, directory, filename):
+    cardHeight = int(imageHeight / rows)
+    cardWidth = int(imageWidth / cols)
+    print(rows)
+    print(cols)
+
     cardCol = 0
     cardRow = 0
     count = 0
     imgData = numpy.asarray(image)
     page = numpy.ones((cardHeight * 3 + 2, cardWidth * 3 + 2, 3)) * 255
-    for i in range(7):
-        for j in range(10):
+    for i in range(rows):
+        for j in range(cols):
             card = imgData[
                 i * cardHeight : (i + 1) * cardHeight,
                 j * cardWidth : (j + 1) * cardWidth,
@@ -59,7 +95,7 @@ def separateImage(image, directory, filename):
                 ] = card
                 cardCol, cardRow = nextCardLocation(cardCol, cardRow)
             if (cardCol == 0 and cardRow == 0) or (
-                i == 7 - 1 and j == 10 - 1
+                i == rows - 1 and cols == 10 - 1
             ):  # if filled up image or about to exit
                 imagePage = Image.fromarray(page.astype(numpy.uint8))
                 imagePage.save(
@@ -70,54 +106,69 @@ def separateImage(image, directory, filename):
     return count
 
 
-def getImages(directory):
-    imageList = []
-    for filename in os.listdir(directory):
-        if re.search(".*\.png", filename) or re.search(".*\.jpeg", filename):
-            imagePath = os.path.join(directory, filename)
-            image = Image.open(imagePath)
-            if checkSize(image):
-                imageList.append(filename)
-    return imageList
-
-
-def buttonPress():
-    imageList = getImages(top.dir)
+def separate():
+    imageList = top.imageList
     num = 0
-    for filename in imageList:
-        imagePath = os.path.join(top.dir, filename)
-        image = Image.open(imagePath)
-        num += separateImage(image, top.dir, filename)
-    tk.messagebox.showinfo("Done", "Create " + str(num) + " Images")
+    for i, filename in enumerate(imageList):
+        cols = int(top.dimCol[i].get())
+        rows = int(top.dimRow[i].get())
+        if cols > 0 and rows > 0:
+            imagePath = os.path.join(top.dir, filename)
+            image = Image.open(imagePath)
+            num += separateImage(image, top.dir, filename, rows, cols)
+    tk.messagebox.showinfo("Done", "Created " + str(num) + " Images")
 
 
-def selectDir(top):
-    top.dir = tk.filedialog.askdirectory(initialdir="/", title="Select Directory",)
-    text.delete(1.0, tk.END)
-    text.insert(tk.INSERT, "dir: " + top.dir)
-    text.insert(tk.INSERT, "\nImages to Separate: ")
-    for image in getImages(top.dir):
-        text.insert(tk.INSERT, "\n" + image)
-    text.pack()
+def setupSelector():
+    tk.Label(top.dirMsg, text="Dir: " + top.dir).grid(row=0)
+
+    imageSelector = top.imageSelector
+    for child in imageSelector.winfo_children():
+        child.destroy()
+    getImages()
+    dimCol = []
+    dimRow = []
+    tk.Label(imageSelector, text="rows").grid(row=0, column=1)
+    tk.Label(imageSelector, text="columns").grid(row=0, column=2)
+    for i, imageName in enumerate(top.imageList):
+        tk.Label(imageSelector, text=imageName).grid(row=i + 1)
+        dimCol.append(tk.Entry(imageSelector))
+        dimRow.append(tk.Entry(imageSelector))
+        dimCol[i].grid(row=i + 1, column=2)
+        dimRow[i].grid(row=i + 1, column=1)
+        dimCol[i].insert(10, 0)
+        dimRow[i].insert(10, 0)
+    top.dimCol = dimCol
+    top.dimRow = dimRow
 
 
-def useCurDir(top):
-    top.dir = os.getcwd()
-    text.delete(1.0, tk.END)
-    text.insert(tk.INSERT, "dir: " + top.dir)
-    text.insert(tk.INSERT, "\nImages to Separate: ")
-    for image in getImages(top.dir):
-        text.insert(tk.INSERT, "\n" + image)
-    text.pack()
+# top.dirmessage = False
+top.imageSelector = tk.Frame(top)
+top.dirMsg = tk.Frame(top)
 
 
-sep = tk.Button(top, text="Separate Images", command=buttonPress)
-select = tk.Button(top, text="Select Directory", command=lambda: selectDir(top))
-useCur = tk.Button(top, text="Use Current Directory", command=useCurDir(top))
+comButtons = tk.Frame(top)
+sep = tk.Button(top, text="Separate Images", command=lambda: separate())
+helpButton = tk.Button(
+    top,
+    text="Help",
+    command=lambda: tk.messagebox.showinfo(
+        "Instructions",
+        "1) Select your desired directory\n2) Input each image's card dimensions\n3) Leave 0,0 for images you wish to ignore \nNote: images with names that start with 'sep' are ignored",
+    ),
+)
+sep.pack(in_=comButtons, side=tk.LEFT)
+helpButton.pack(in_=comButtons, side=tk.RIGHT)
 
-dirButtons.pack()
+dirButtons = tk.Frame(top)
+select = tk.Button(top, text="Select Directory", command=lambda: selectDir())
+useCur = tk.Button(top, text="Use Current Directory", command=useCurDir())
 select.pack(in_=dirButtons, side=tk.LEFT)
 useCur.pack(in_=dirButtons, side=tk.RIGHT)
-sep.pack()
+
+top.dirMsg.pack()
+top.imageSelector.pack()
+comButtons.pack()
+dirButtons.pack()
 
 top.mainloop()
